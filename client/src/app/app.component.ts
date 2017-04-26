f789116b385a1b063767877fc052c804ed8404e3
aisav <artur.isaverdyan@gmail.com>
2017-04-26 11:42:29 +0400

    book.service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [BookService]
 })
 export class AppComponent {
+  constructor(private bookService:BookService){
+    
+  }
+
   title1 = 'Books List';
   title2 = 'Favorite Books';
+
+  
 }
--- client/src/app/book.service.ts
+++ client/src/app/book.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class BookService {
-    constructor() { }
+    constructor(private http: http) {
+        console.log("================   BookServise =============")
+     }
+
+     getBooks(){
+         return this.http.get('http://localhost:3000/api/books')
+                        .map(res => res.json());
+     }
 }
